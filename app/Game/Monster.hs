99ed274eada9b10fce83d4234e9fe3e1695f9c85
AdneMoretti <morettiadne1@gmail.com>
2023-09-17 20:47:42 -0300

    feat: adding initial monster module

---
+++ app/Game/Monster.hs
@@ -0,0 +1,7 @@
+module Monster (nextPositionBfs) where 
+import Types 
+
+nextPositionBfs:: GameState -> Position
+
+-- nextPositionBfs state = state
+-- nextPositionDijskstra state = state
---
+++ app/Types.hs
@@ -0,0 +1,15 @@
+module Types (CellState, GameState, Position) where 
+
+data CellState 
+  = Wall 
+  | Goal 
+  | Empty
+
+type GameState = 
+  {
+  , gameMap :: map Position CellState
+  , playerPosition :: Position
+  , enemyPosition :: Position
+  }
+
+type Position = (Int, Int)

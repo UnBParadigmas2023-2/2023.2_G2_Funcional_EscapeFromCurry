461fb28f68965388385ffec9447fbf991023b836
gabriel costa <gabriel.costa513@gmail.com>
2023-09-18 13:09:45 -0300

    :construction: feat: iniciando funções do módulo do monstro
    Co-authored-by: Adne Moretti <morettiadne1@gmail.com>

---
+++ src/Monster.hs
@@ -0,0 +1,27 @@
+module Monster () where
+
+import Types
+
+import qualified Data.Map as Map
+import qualified Data.Queue as Queue
+import Data.Map (empty)
+
+
+bfs :: GameState -> Queue []  ->  Map Position Position 
+bfs gameState father queue  = do 
+  
+
+nextPosition :: Map.Map Position Position -> Position -> Position -> Position -> Position
+nextPosition father currentPosition monsterPosition playerPosition
+    | fatherPosition == error = monsterPosition
+    | fatherPosition == monsterPosition = currentPosition
+    | otherwise = nextPosition father fatherPosition monsterPosition playerPosition
+  where
+    fatherPosition = father ! currentPosition
+
+
+
+nextPositionBFS :: GameState -> Position 
+nextPositionBFS gameState = nextPosition ( bfs(gameState [gameState.enemyPosition] Map.empty ) gameState.playerPosition gameState.enemyPosition gameState.playerPosition)
+
+  

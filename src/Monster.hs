90937bd96b52e1ab6dd510cb2e97e8705955a2b7
gabriel costa <gabriel.costa513@gmail.com>
2023-09-19 09:58:25 -0300

    :construction: feat: colocando bfs bugada
    Co-authored-by: Adne Moretti <morettiadne1@gmail.com>

--- src/Monster.hs
+++ src/Monster.hs
@@ -1,37 +1,36 @@
 module Monster () where
 
-import Types ( GameState, Position, CellState (..), GameMap )
+import Types ( GameState, Position, CellState (..), GameMap, neighborsFor )
 
 import qualified Data.Map as Map
 
-validPosition :: GameMap -> Position -> Bool
-validPosition gameMap currentPosition =
+validPosition :: GameMap -> Map.Map Position Position -> Position -> Bool
+validPosition gameMap father currentPosition =
     case Map.lookup currentPosition gameMap of
-        Just current -> current == Empty
-        Nothing -> False
-
--- insertPosition :: GameState -> [Position] -> Position -> Direction -> [Position]
--- insertPosition :: GameState -> Position -> [Position] -> [Position]
-addPosition :: Position -> Position -> Position
-addPosition (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+        Just wall -> wall == Empty &&
+            case Map.lookup currentPosition father of
+                Just _ -> False  
+                Nothing -> True  
+        Nothing -> False 
 
 bfs :: GameState ->  [Position] ->  Map.Map Position Position -> Map.Map Position Position
 bfs  _ [] father = father
-bfs gameState (x:ys) father = do 
-  right <- currentPosition `addPosition` directionOffset DirRight
-  gameState.gameMap ! right
-
+bfs gameState (v:xs) father = 
+    let validNeighbors = filter (validPosition gameMap father v) neighborsFor v
+        newFather = Map.fromList [(n, v) | n <- validPosition]
+        newQueue = xs ++ validNeighbors
+    in bfs gameState newQueue (Map.union father newFather) 
   
 nextPosition :: Map.Map Position Position -> Position -> Position -> Position -> Position
-nextPosition father currentPosition monsterPosition playerPosition
-    | fatherPosition == error = monsterPosition
-    | fatherPosition == monsterPosition = currentPosition
-    | otherwise = nextPosition father fatherPosition monsterPosition playerPosition
-  where
-    fatherPosition = father ! currentPosition
+nextPosition father currentPosition monsterPosition playerPosition =
+  case Map.lookup currentPosition father of
+    Just fatherPosition
+      | fatherPosition == monsterPosition -> currentPosition
+      | otherwise -> nextPosition father fatherPosition monsterPosition playerPosition
+    Nothing -> currentPosition 
 
-
-nextPositionBFS :: GameState -> Position 
-nextPositionBFS gameState = nextPosition ( bfs(gameState [gameState.enemyPosition] Map.empty ) gameState.playerPosition gameState.enemyPosition gameState.playerPosition)
-
-  
+nextPositionBFS :: GameState -> Position
+nextPositionBFS gameState =
+  let defaultFather = Map.singleton (enemyPosition gameState) (666, 666)
+      bfsResult = bfs gameState [gameState.enemyPosition] defaultFather
+  in nextPosition bfsResult gameState.playerPosition gameState.enemyPosition gameState.playerPosition

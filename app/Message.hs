75a33b2cc0a6ae591590a39a446d9e3fc80be985
luclopesr <luclopesr@gmail.com>
2023-09-19 10:27:06 -0300

    Adicionando módulo Message
    Co-authored-by: joao-henrique10 <jhcalzavara@hotmail.com>

--- app/Message.hs
+++
@@ -1,9 +0,0 @@
-module Message (mensagemParabens, mensagemIncentivo) where
-
-import Graphics.Gloss
-
-mensagemParabens :: Picture
-mensagemParabens = Translate (-200) 0 $ Scale 0.5 0.5 $ Text "Parabéns! Você concluiu o labirinto com animações."
-
-mensagemIncentivo :: Picture
-mensagemIncentivo = Translate (-200) 0 $ Scale 0.5 0.5 $ Text "Ops! Você não conseguiu, tente novamente!"
---
+++ src/Message.hs
@@ -0,0 +1,19 @@
+module Message where
+
+import Graphics.Gloss
+
+checkResult:: Bool -> IO ()
+
+checkResult True =
+  Pictures
+    [ Translate (-200) 0 $ Scale 0.5 0.5 $ Color green $ Text "Parabéns! Você venceu o jogo!",
+      Translate (-100) (-100) $ Scale 0.3 0.3 $ Color blue $ Text "Pressione 'P' para jogar novamente.",
+      Translate (-100) (-150) $ Scale 0.3 0.3 $ Color yellow $ Text "Pressione 'M' para voltar ao menu."
+    ]
+    
+checkResult False =
+  Pictures
+    [ Translate (-200) 0 $ Scale 0.5 0.5 $ Color red $ Text "Poxa, o monstro te pegou! Se desafie, tente novamente!",
+      Translate (-100) (-100) $ Scale 0.3 0.3 $ Color blue $ Text "Pressione 'P' para jogar novamente.",
+      Translate (-100) (-150) $ Scale 0.3 0.3 $ Color yellow $ Text "Pressione 'M' para voltar ao menu."
+    ]

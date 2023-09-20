e217c1dea7ae551f5c51c565ba5cba388a40a0ed
caiozim112 <caioopua@gmail.com>
2023-09-20 15:05:51 -0300

    criação do mapa

---
+++ src/Map.hs
@@ -0,0 +1,46 @@
+
+
+import qualified Data.Map.Strict as M
+
+import Types (CellState (..), GameMap, Position, GameState (..), neighborsFor, offsets)
+import qualified System.Random as R
+import Generator (mkMaze, genMaze, showMaze)
+import Graphics.Gloss
+
+
+-- Definir aqui o Mapa Obrigatorio
+-- a :: GameState
+-- a = let
+--     mwidth = 40
+--     mheight = 40
+--     maze = mkMaze mwidth mheight
+--     fullMaze = genMaze maze (0, 0) (R.mkStdGen 100)
+--     gs = GameState { 
+--       gameMap = fullMaze, width = mwidth, height = mheight, playerPosition = (0, 0), enemyPosition = (0, 0) 
+--     }
+--   in gs
+
+cellStateToPicture :: CellState -> Picture
+cellStateToPicture Wall = color wallColor $ rectangleSolid cellSize cellSize
+cellStateToPicture Empty = color emptyColor $ rectangleSolid cellSize cellSize
+cellStateToPicture Goal = color goalColor $ rectangleSolid cellSize cellSize
+
+wallColor = black
+emptyColor = white
+goalColor = green
+cellSize = 10
+
+displayGameMap :: GameState -> Picture
+displayGameMap gameState =
+  pictures [translate (fromIntegral x * cellSize) (fromIntegral y * cellSize) (cellStateToPicture cellState)
+            | ((x, y), cellState) <- gameMapWithCoords]
+  where
+    gameMapWithCoords = [((x, y), M.findWithDefault Wall (x, y) (gameMap gameState))
+                         | x <- [0 .. width gameState - 1],
+                           y <- [0 .. height gameState - 1]]
+
+
+main :: IO ()
+main = do
+    let gameState = a  -- Assuming 'a' is your GameState initialization
+    display (InWindow "Game Map" (1980, 1020) (0, 0)) white (displayGameMap gameState)
